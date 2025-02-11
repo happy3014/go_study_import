d2c9dd2c8a59690d2fbc11cf22c9ccee3b4d3f1e
happylv <happylv@tencent.com>
2025-02-11 11:07:52 +0800

    [feature] 增加新的版本

--- study1.go
+++ study1.go
@@ -17,3 +17,7 @@ func PrintFunc3() {
 func PrintFunc4() {
 	fmt.Println("this is in go_study_import study1 func4")
 }
+
+func PrintFunc5() {
+	fmt.Println("this is in go_study_import study1 func5")
+}
